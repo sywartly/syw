795a571e5f47659a5656c99ce7c3a681324eb039
SywArtly <3393727716@qq.com>
2024-12-24 10:09:00 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,83 @@
+
+import VueRouter from 'vue-router';
+
+const routes = [
+
+    {
+        path:'/',
+        name:'login',
+        component:()=>import('../components/Login')
+    },
+    {
+        path:'/Index',
+        name:'index',
+        component:()=>import('../components/Index'),
+        children:[
+            {
+                path:'/Home',
+                name:'home',
+                meta:{
+                    title:'首页'
+                },
+                component:()=>import('../components/Home')
+            },
+            {
+                path: '/Storage',
+                name: 'storage',
+                meta: {
+                    title: '地区管理'
+                },
+                component: () => import('../components/storage/StorageManage.vue')
+            },
+            {
+                path: '/Goodstype',
+                name: 'goodstype',
+                meta: {
+                    title: '地区管理'
+                },
+                component: () => import('../components/goodstype/GoodstypeManage.vue')
+            },
+            {
+                path: '/Goods',
+                name: 'goods',
+                meta: {
+                    title: '地区管理'
+                },
+                component: () => import('../components/goods/GoodsManage.vue')
+            },
+            /*{
+                path:'/Admin',
+                name:'admin',
+                meta:{
+                    title:'管理员管理'
+                },
+                component:()=>import('../components/admin/AdminManage.vue')
+            },
+            {
+                path:'/User',
+                name:'user',
+                meta:{
+                    title:'用户管理'
+                },
+                component:()=>import('../components/user/UserManage.vue')
+            },*/
+        ]
+    }
+]
+
+const router = new VueRouter({
+    mode:'history',
+    routes
+})
+
+export function resetRouter() {
+    router.matcher = new VueRouter({
+        mode:'history',
+        routes: []
+    }).matcher
+}
+const VueRouterPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (to) {
+    return VueRouterPush.call(this, to).catch(err => err)
+}
+export  default router;
